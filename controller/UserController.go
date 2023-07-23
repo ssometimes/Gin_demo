373333a02c98ae3620b83c87be2f25a19e5e6ee0
lian <1185339869@qq.com>
2023-07-24 07:59:42 +0800

    增加登录接口

--- controller/UserController.go
+++ controller/UserController.go
@@ -5,6 +5,7 @@ import (
 	"OceanLearn/model"
 	"OceanLearn/util"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
@@ -47,12 +48,20 @@ func Register(ctx *gin.Context) {
 		return
 	}
 
+	hasePassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"code":    500,
+			"message": "加密错误",
+		})
+		return
+	}
 	// 创建新的对象进行存储
 	user := model.User{
 		Model:     gorm.Model{},
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasePassword),
 	}
 
 	DB.Create(&user)
@@ -61,6 +70,59 @@ func Register(ctx *gin.Context) {
 		"code":    200,
 		"message": name + " 注册成功",
 	})
+}
+
+func Login(ctx *gin.Context) {
+	DB := common.GetDb()
+	// 获取参数
+	name := ctx.PostForm("name")
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+
+	// 数据校验
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "手机必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "密码少于6位",
+		})
+		return
+	}
+
+	// 查询手机号是否存在
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "用户不存在",
+		})
+		return
+	}
+
+	// 验证密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    400,
+			"message": "密码错误",
+		})
+		return
+	}
+
+	// 发放token
+	token := "tokenData"
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":    200,
+		"data":    gin.H{"token": token},
+		"message": name + ": 登录成功",
+	})
+	return
 }
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
--- routes.go
+++ routes.go
@@ -8,5 +8,6 @@ import (
 // POST此类函数是将api添加到一个组里面，所以调用后返回这个 gin.Engine就获得了所有方法
 func CollectRoute(r *gin.Engine) *gin.Engine {
 	r.POST("/api/auth/register", controller.Register)
+	r.POST("/api/auth/login", controller.Login)
 	return r
 }
