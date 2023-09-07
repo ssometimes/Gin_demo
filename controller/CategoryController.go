83a02be7f120edb56762aecf9dbf5f0896ce3b23
lian <1185339869@qq.com>
2023-09-07 08:09:45 +0800

    1. 完成文章分类功能 2. 完善思维导图

--- controller/CategoryController.go
+++ controller/CategoryController.go
@@ -45,6 +45,7 @@ func (c CategoryController) Update(ctx *gin.Context) {
 	ctx.ShouldBind(requestCategory)
 	if requestCategory.Name == "" {
 		response.Fail(ctx, nil, "数据验证错误，分类名称必填")
+		return
 	}
 
 	// 创建对象，查找数据库并赋值给对象
@@ -55,6 +56,7 @@ func (c CategoryController) Update(ctx *gin.Context) {
 	// 查询错误会返回 ErrRecordNotFound 错误
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		response.Fail(ctx, nil, "分类不存在")
+		return
 	}
 
 	c.DB.Model(&updateCategory).Updates(gin.H{"name": requestCategory.Name})
@@ -63,11 +65,29 @@ func (c CategoryController) Update(ctx *gin.Context) {
 }
 
 func (c CategoryController) Show(ctx *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	var requestCategory model.Category
+	ctx.ShouldBind(requestCategory)
+	if requestCategory.Name == "" {
+		response.Fail(ctx, nil, "数据验证错误，分类名称必填")
+	}
+
+	var category model.Category
+	categoryID, _ := strconv.Atoi(ctx.Params.ByName("id"))
+	result := c.DB.First(&category, categoryID)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		response.Fail(ctx, nil, "分类不存在")
+		return
+	}
+	response.Success(ctx, gin.H{"category": category}, "")
 }
 
 func (c CategoryController) Delete(ctx *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	categoryID, _ := strconv.Atoi(ctx.Params.ByName("id"))
+	// 根据主键删除，指定表格和主键值，DELETE FROM users WHERE id = 10;
+	if err := c.DB.Delete(model.Category{}, categoryID); err != nil {
+		response.Fail(ctx, nil, "删除失败")
+		return
+	}
+	response.Success(ctx, nil, "删除成功")
 }
